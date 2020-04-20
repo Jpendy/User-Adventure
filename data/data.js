3ac7d276bbf602b68e5cfdcfd05020c4f93be010
Jpendy <Jpendy256@gmail.com>
2020-04-19 21:21:07 -0700

    added two more story lines

--- data/data.js
+++ data/data.js
@@ -14,17 +14,17 @@ const Walker = {
         id: 'negotiate',
         description: 'Offer to trade with it',
         result: `
-             You offer to trade with it and it smiles a sickly smile, the nods. It suddenly reaches 
-             forward and snatches your wrist, while it raises it's other hand. You see a flash of shiny metal 
-             a feel a sudden blinding pain and before you know it the White Walker is running off, in 
-             posession of your left hand. It drops a bag as you stare in horror at the bloody ruin of your wrist. You open
+             You offer to trade with it and it smiles a sickly smile, then nods. It suddenly reaches 
+             forward and snatches your wrist, while it raises its other hand. You see a flash of shiny metal 
+             and feel a sudden blinding pain and before you know it the White Walker is running off, in 
+             possession of your left hand. It drops a bag as you stare in horror at the bloody ruin of your wrist. You open
              the bag and find 25 gold in it. You look around as the snow falls, and you are alone, once again. You lose 45 hp.
         `,
         hp: -45,
         gold: 25
     }, {
         id: 'Attack',
-        description: `Pull out your sword and aim a vicious attack at it's face!`,
+        description: `Pull out your sword and aim a vicious attack at its face!`,
         result: `
             The White Walker moves quickly and blocks your sword with his own, shattering
             your blade into a thousand pieces. As you stare dumbly at the remains of your 
@@ -49,140 +49,152 @@ const Walker = {
     }]
 };
 
-const dragon = {
+const Dragon = {
     id: 'dragon',
-    title: 'A Problem Dragon',
+    title: 'A Dragon Princess',
     map: {
-        top: '60%',
-        left: '50%'
+        top: '56.6%',
+        left: '50.5%'
     },
     image: 'dragon.jpg',
-    audio: 'dragon.wav',
-    action: 'dragon-growl.aiff',
+    // audio: '.wav',
+    // action: '.aiff',
     description: `
-        You travel to a nearby village you have heard is being
-        terrorized by a dragon. Sure enough as you rent a room
-        in a local inn, you go outside and see the dragon about
-        to lay seige! What do you do?
+        As you ride next to a large river, a huge dragon swoops down from the sky and blocks your path.
+        A woman climbs down from on top of its back and tells you she is your
+        Queen and you must bend the knee. She also says she is your mother, which
+        makes no sense and is kind of weird. What do you do?
     `,
     choices: [{
-        id: 'run',
-        description: 'Get the hell out of the village',
+        id: 'Bend',
+        description: 'Bend the knee.',
         result: `
-            You high tail it in the opposite direction. Luckily,
-            in the panic you find a bag on the ground with 15 gold.
-            Unluckily, you trip over a discarded wagon wheel on your
-            way out of town and take 35 hp damage. 
-        `,
-        hp: -35,
-        gold: 15
-    }, {
-        id: 'fight',
-        description: 'Fiiiiiggghhhttt!',
-        result: `
-            You attempt to charge towards the dragon, who sees you approach
-            and let's loose a fireball. You wake up the next morning and the
-            village has been completely burned to the ground.
-            Oh, and you take 45 hp damage.
-        `,
-        hp: -45,
-        gold: 0
-    }, {
-        id: 'archer',
-        description: 'Emulate that guy from LOR who shot an arrow',
-        result: `
-            Inspired by the legend of Bard the Bowman, you notice a
-            stunned archer standing nearby and take their bow and quiver,
-            climb to the top of a tall tower and take aim. On the dragon's
-            next pass you steady your aim and let one fly. Amazingly,
-            you strike the dragon in the eye, piercing into the brain and
-            killing the dragon instantly. The villagers declare you their hero
-            and award you 90 gold.
+            You climb down from your horse and kneel down, lowering your head. "My queen!" You 
+            say, with respect and deference. The queen then tells you that you owe 20 gold as taxes.
+            Looking at the dragon's large teeth, you dig into your purse and pay up.
         `,
         hp: 0,
-        gold: 90
+        gold: -20
+    }, {
+        id: 'Stand',
+        description: '"Tell her that you kneel for no man, or woman, ever. Not even for mom.."',
+        result: `
+           The woman gets a dark angry look and and lets out a bitter, pitiless laugh. "Do you hear bells 
+           ringing" she askes, before suddenly screaming "DROGON! DRACARYS!!" The dragon roars and shoots a
+           gigantic fireball straight towards you, completely enveloping both you and your horse. You feel
+           horrible heat, then nothing at all..
+        `,
+        hp: -1000,
+        gold: 0
+    }, {
+        id: 'Spear Attack',
+        description: 'Throw you spear at the woman',
+        result: `
+            Angered by her demands, you immediately heft your spear and throw it with all your might towards
+            the crazy woman. Your throw takes her directly in the face, killing her instantly. The dragon roars 
+            and lunges at you, scaring your horse into bucking and throwing you into the river. As the dragon 
+            tears your horse apart you are carried down river. When you finally surface 40 seconds later, you 
+            are out of sight. You survive while sustaining only minor injuries, doing you 10 damage. You also spotted
+            a small chest in the river, and inside it you found 30 gold.
+        `,
+        hp: -10,
+        gold: 30
     }]
 };
 
-const treasure = {
-    id: 'treasure',
-    title: 'The Golden Treasure',
+const Combat = {
+    id: 'fight',
+    title: `The Half-Man's Fight`,
     map: {
-        top: '34%',
-        left: '45%'
+        top: '51%',
+        left: '63%'
     },
-    image: 'treasure-chests.png',
-    audio: 'treasure-chests.wav',
-    action: 'chest-opening.wav',
+    image: '',
+    // audio: '.wav',
+    // action: '.wav',
     description: `
-        As you enter the quest chamber you notice three chests before you.
-        Just as you start to imagine the wealth, you see a giant serpent
-        emerge from the back of the chamber. You'll need to make a run for it,
-        but you have time to open one chest before you take off. Which one 
-        do you choose?
+        While visiting the Eyrie, you happen to be witness a trial that was ongoing, while you are there,
+        the accused, a Lord Tyrion Lannister, asks desperately 
+        for anyone present to be his champion, and fight for his freedom and his cause. What do you do?
     `,
     choices: [{
-        id: 'wooden',
-        description: 'A Wooden Chest',
-        result: 'You grab 40 gold pieces!',
+        id: 'Nothing',
+        description: 'Do and say nothing, just watch',
+        result: `
+        Realizing no one will help him, Tyrion suddenly grabbed a weapon from a nearby guard 
+        and begins running for the door, slashing at two guards. as he runs by you, he stabs you in the
+        arm, doing 40 damage.
+        `,
+        hp: -40,
+        gold: 0
+    }, {
+        id: 'fight',
+        description: 'Stand and declare yourself the champion',
+        result: `
+        You stand in single combat against a large heavily armored knight. The armor is too strong for 
+        you, and you cannot break through it. After 90 seconds of combat, the knights suddenly moves
+        much faster than he was and cleanly sweeps his blade up and through your neck, sending your head
+        flying into the courtyard. two minutes later Lord Tyrion is thrown through the Moon Door to his death.
+        `,
+        hp: -1000,
+        gold: 0
+    }, {
+        id: 'Sneak',
+        description: 'Attempt to be sneaky',
+        result: `
+        You declare yourself the champion, then demand the fight take place the next morning at dawn,
+        so you may take the evening to fulfill the sacred fight ceremonies that your religion demands.
+        during the night, you drink with the guards and when they are distracted you unlock the door
+        to Tyrion Lannisters prison cell. The two of you escape into the night. Tyrion rewards you later
+        300 gold.
+        `,
         hp: 0,
-        gold: 40
-    }, {
-        id: 'golden',
-        description: 'A Golden Chest',
-        result: 'Oh no! The chest is booby trapped with poison and you take 50 hp damage',
-        hp: -50,
-        gold: 0
-    }, {
-        id: 'jeweled',
-        description: 'A Jeweled Chest',
-        result: 'A warm light engulfs you and you gain 35 hp',
-        hp: 35,
-        gold: 0
+        gold: 300
     }]
 };
 
 const quests = [
     Walker, 
-    treasure,
-    dragon, {
-        id: 'pleasure',
-        title: 'The Bold Folding Pleasure',
-        map: {
-            top: '58%',
-            left: '25%'
-        },
-        image: 'treasure-chests.png',
-        audio: 'treasure-chests.wav',
-        action: 'chest-opening.wav',
-        description: `
-        As you enter the quest chamber you notice three chests before you.
-        Just as you start to imagine the wealth, you see a giant serpent
-        emerge from the back of the chamber. You'll need to make a run for it,
-        but you have time to open one chest before you take off. Which one 
-        do you choose?
-    `,
-        choices: [{
-            id: 'wooden',
-            description: 'WOW',
-            result: 'You grab 40 gold pieces!',
-            hp: 0,
-            gold: 40
-        }, {
-            id: 'golden',
-            description: 'NOW',
-            result: 'Oh no! The chest is booby trapped with poison and you take 50 hp damage',
-            hp: -50,
-            gold: 0
-        }, {
-            id: 'jeweled',
-            description: 'POW',
-            result: 'A warm light engulfs you and you gain 35 hp',
-            hp: 35,
-            gold: 0
-        }
-        ]
-    }
+    Combat,
+    Dragon 
+    //, {
+    // //     id: 'pleasure',
+    // //     title: 'The Bold Folding Pleasure',
+    // //     map: {
+    // //         top: '58%',
+    // //         left: '25%'
+    // //     },
+    // //     image: '',
+    // //     audio: '.wav',
+    // //     action: '.wav',
+    // //     description: `
+    // //     As you enter the quest chamber you notice three chests before you.
+    // //     Just as you start to imagine the wealth, you see a giant serpent
+    // //     emerge from the back of the chamber. You'll need to make a run for it,
+    // //     but you have time to open one chest before you take off. Which one 
+    // //     do you choose?
+    // // `,
+    // //     choices: [{
+    // //         id: 'wooden',
+    // //         description: 'WOW',
+    // //         result: 'You grab 40 gold pieces!',
+    // //         hp: 0,
+    // //         gold: 40
+    // //     }, {
+    // //         id: 'golden',
+    // //         description: 'NOW',
+    // //         result: 'Oh no! The chest is booby trapped with poison and you take 50 hp damage',
+    // //         hp: -50,
+    // //         gold: 0
+    // //     }, {
+    // //         id: 'jeweled',
+    // //         description: 'POW',
+    // //         result: 'A warm light engulfs you and you gain 35 hp',
+    // //         hp: 35,
+    // //         gold: 0
+    // //     }
+    // //     ]
+    // }
 ];
 
 export default quests;
