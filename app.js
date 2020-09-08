87b3dfff5303e32c1ab746ffb3afb83867f1cbbc
Jpendy <Jpendy256@gmail.com>
2020-09-08 16:27:57 -0700

    changed path

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ myForm.addEventListener('submit', (event) => {
     const stringifiedUser = JSON.stringify(user);
     localStorage.setItem('USER', stringifiedUser);
 
-    location.href = '/user-adventure/map/';
+    location.href = '/User-Adventure/map/';
 });
 
 
